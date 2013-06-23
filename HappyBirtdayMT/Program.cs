44bdbdbf8f2b42ffde76623d2656ffac17ee8317
Øyvind Sandåker <mr.potet@gmail.com>
2013-06-23 08:09:40 +0200

    Happy birthday

---
+++ HappyBirtdayMT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HappyBirtdayMT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 3; i++)
+            {
+                Console.WriteLine(((i == 2) ? "Gratulerer kjære Morten..!" : "Gratulerer med dagen!"));
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("This message will self destruct in 5 sec.");
+            Thread.Sleep(5000);
+        }
+    }
+}
